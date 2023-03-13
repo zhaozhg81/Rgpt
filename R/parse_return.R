d0900336ed090fcf1fd149264e8ba678ab36174e
Zhigen Zhao <zhaozhg@guoliang.guoliang>
2023-03-13 12:22:50 -0400

    update

---
+++ R/parse_return.R
@@ -0,0 +1,21 @@
+#' Title
+#'
+#' @param return
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#'
+#'
+#'
+parse_return <-
+function(return) {
+
+    if (grepl("gpt-3.5-turbo", Sys.getenv("OPENAI_MODEL", "gpt-3.5-turbo"))) {
+    result = trimws(sapply(return$choices, function(x) x$message$content))
+  } else {
+    result= trimws(sapply(return$choices, function(x) x$text))
+  }
+  result
+}
